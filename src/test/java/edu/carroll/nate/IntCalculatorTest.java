c88bfedd8462591de75524339312a2dd5e6b7602
Nate Williams <nate@yogotech.com>
2021-10-11 12:55:40 -0600

    Add a second assertion for the singleton

--- src/test/java/edu/carroll/nate/IntCalculatorTest.java
+++ src/test/java/edu/carroll/nate/IntCalculatorTest.java
@@ -1,11 +1,19 @@
 package edu.carroll.nate;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import org.junit.Test;
 
 public class IntCalculatorTest {
     @Test
-    public void firstTest() {
+    public void singletonTest() {
 	assertNotNull(IntCalculator.getInstance());
+    }
+
+    @Test
+    public void singletonIdentical() {
+        IntCalculator instance1 = IntCalculator.getInstance();
+	assertNotNull(instance1);
+	assertEquals(instance1, IntCalculator.getInstance());
     }
 }
