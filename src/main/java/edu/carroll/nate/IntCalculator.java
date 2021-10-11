10d27d13e22045f92f345d3fbd1acaab7ea5db9f
Nate Williams <nate@yogotech.com>
2021-10-11 13:37:46 -0600

    Formatting cleanup pass

--- src/main/java/edu/carroll/nate/IntCalculator.java
+++ src/main/java/edu/carroll/nate/IntCalculator.java
@@ -6,26 +6,26 @@ public class IntCalculator {
     public static IntCalculator getInstance() {
         if (instance == null) {
             instance = new IntCalculator();
-	}
-	return instance;
+        }
+        return instance;
     }
 
     private IntCalculator() {
     }
 
     public int add(int num1, int num2) {
-	return num1 + num2;
+        return num1 + num2;
     }
 
     public int subtract(int num1, int num2) {
-	return num1 + num2;
+        return num1 + num2;
     }
 
     public int multiple(int num1, int num2) {
-	return num1 * num2;
+        return num1 * num2;
     }
 
     public int divide(int num1, int num2) {
-	return num1 / num2;
+        return num1 / num2;
     }
 }
--- src/test/java/edu/carroll/nate/IntCalculatorTest.java
+++ src/test/java/edu/carroll/nate/IntCalculatorTest.java
@@ -1,69 +1,70 @@
 package edu.carroll.nate;
 
+import org.junit.Test;
+
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertNotNull;
-import org.junit.Test;
 
 public class IntCalculatorTest {
     @Test
     public void singletonTest() {
-	assertNotNull(IntCalculator.getInstance());
+        assertNotNull(IntCalculator.getInstance());
     }
 
     @Test
     public void singletonIdentical() {
         IntCalculator instance1 = IntCalculator.getInstance();
-	assertNotNull(instance1);
-	assertEquals(instance1, IntCalculator.getInstance());
+        assertNotNull(instance1);
+        assertEquals(instance1, IntCalculator.getInstance());
     }
-    
+
     @Test
     public void addPositiveIdentical() {
         IntCalculator calc = IntCalculator.getInstance();
-	assertNotNull(calc);
-	assertEquals(4, calc.add(2, 2));
+        assertNotNull(calc);
+        assertEquals(4, calc.add(2, 2));
     }
 
     @Test
     public void addPositiveDifferent() {
         IntCalculator calc = IntCalculator.getInstance();
-	assertNotNull(calc);
-	assertEquals(3, calc.add(1, 2));
+        assertNotNull(calc);
+        assertEquals(3, calc.add(1, 2));
     }
 
     @Test
     public void addPositiveAndZero() {
         IntCalculator calc = IntCalculator.getInstance();
-	assertNotNull(calc);
-	assertEquals(2, calc.add(0, 2));
+        assertNotNull(calc);
+        assertEquals(2, calc.add(0, 2));
     }
 
     @Test
     public void addAllZero() {
         IntCalculator calc = IntCalculator.getInstance();
-	assertNotNull(calc);
-	assertEquals(0, calc.add(0, 0));
+        assertNotNull(calc);
+        assertEquals(0, calc.add(0, 0));
     }
 
     @Test
     public void addNegativeZero() {
         IntCalculator calc = IntCalculator.getInstance();
-	assertNotNull(calc);
-	assertEquals(0, calc.add(-0, 0));
+        assertNotNull(calc);
+        assertEquals(0, calc.add(-0, 0));
     }
 
     @Test
     public void addTwoNegativeZero() {
         IntCalculator calc = IntCalculator.getInstance();
-	assertNotNull(calc);
-	assertEquals(0, calc.add(-0, -0));
+        assertNotNull(calc);
+        assertEquals(0, calc.add(-0, -0));
     }
 
     @Test
     public void addSillyFailure() {
         IntCalculator calc = IntCalculator.getInstance();
-	assertNotNull(calc);
-	assertNotEquals(3, calc.add(2, 2));
+        assertNotNull(calc);
+        assertNotEquals(3, calc.add(2, 2));
     }
 }
