208b4ab8d63af5b087f021adb630633c70448e31
Nate Williams <nate@yogotech.com>
2021-10-11 12:58:46 -0600

    Add a bunch more unit tests

--- src/test/java/edu/carroll/nate/IntCalculatorTest.java
+++ src/test/java/edu/carroll/nate/IntCalculatorTest.java
@@ -16,4 +16,46 @@ public class IntCalculatorTest {
 	assertNotNull(instance1);
 	assertEquals(instance1, IntCalculator.getInstance());
     }
+    
+    @Test
+    public void addPositiveIdentical() {
+        IntCalculator calc = IntCalculator.getInstance();
+	assertNotNull(calc);
+	assertEquals(4, calc.add(2, 2));
+    }
+
+    @Test
+    public void addPositiveDifferent() {
+        IntCalculator calc = IntCalculator.getInstance();
+	assertNotNull(calc);
+	assertEquals(3, calc.add(1, 2));
+    }
+
+    @Test
+    public void addPositiveAndZero() {
+        IntCalculator calc = IntCalculator.getInstance();
+	assertNotNull(calc);
+	assertEquals(2, calc.add(0, 2));
+    }
+
+    @Test
+    public void addAllZero() {
+        IntCalculator calc = IntCalculator.getInstance();
+	assertNotNull(calc);
+	assertEquals(0, calc.add(0, 0));
+    }
+
+    @Test
+    public void addNegativeZero() {
+        IntCalculator calc = IntCalculator.getInstance();
+	assertNotNull(calc);
+	assertEquals(0, calc.add(-0, 0));
+    }
+
+    @Test
+    public void addTwoNegativeZero() {
+        IntCalculator calc = IntCalculator.getInstance();
+	assertNotNull(calc);
+	assertEquals(0, calc.add(-0, -0));
+    }
 }
